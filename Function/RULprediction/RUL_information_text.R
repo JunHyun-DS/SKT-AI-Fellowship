716e168ecfed2ad6352db71a6da3d510583c13aa
JunHyun-DS <60165706+JunHyun-DS@users.noreply.github.com>
2022-04-23 19:20:12 +0900

    Update RUL_information_text.R

--- Function/RULprediction/RUL_information_text.R
+++ Function/RULprediction/RUL_information_text.R
@@ -6,23 +6,3 @@ RUL_information_text <- function(current_time, predict_time, rul){
       ,sep=''
   )  
 }
-
-Mixture_information_text <- function(posterior, rul, result, time, model){
-  cat('\n',paste('- Information of Mixture Model -'),'\n\n', sep='')
-  for(i in 1:length(model)){
-    cat(paste('----------', model[i]  ,'------------'),'\n', sep='')
-    cat('percentage: ', round(posterior[i]*100,3), '%','\n',sep='')
-    cat('RUL: ', rul[i], '\n\n',sep='')
-  } 
-  cat(paste('---------- result ----------'),'\n', sep='')
-  cat('current time: ', time, '\n',sep='')
-  cat('RUL: ', round(result,3), '\n\n\n',sep='')
-  
-}
-
-bayesian_iteration_text <- function(bayesian_time, posterior, model){
-  cat('\n', paste('---------- time ', bayesian_time ,' ------------', sep=''),'\n', sep='')
-  for(i in 1:length(model)){
-    cat(model[i], ': ', round(posterior[i]*100,3), '%','\n',sep='')
-  }
-}
